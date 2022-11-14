02f82ed08599e7596653324bff81128416f01fa7
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-11-14 19:31:37 +0100

    Update install-driver.sh

--- install-driver.sh
+++ install-driver.sh
@@ -119,23 +119,4 @@ echo "The driver was installed successfully."
 
 # unblock wifi
 rfkill unblock wlan
-
-# if NoPrompt is not used, ask user some questions to complete installation
-if [ $NO_PROMPT -ne 1 ]
-then
-	read -p "Do you want to edit the driver options file now? [y/N] " -n 1 -r
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]
-	then
-		nano /etc/modprobe.d/${OPTIONS_FILE}
-	fi
-
-	read -p "Do you want to reboot now? (recommended) [y/N] " -n 1 -r
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]
-	then
-		reboot
-	fi
-fi
-
 exit 0
